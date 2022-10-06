87aaff4b74515f62f0254c5aa59492ff5f6f2199
Shahabaj Dange <dangeshahabaj@hotmail.com>
2022-10-06 13:29:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>CSS Shapes [Small CSS tricks]</h1>
+      <h4>Triangle</h4>
+      <div className="triangle-right"/>
+      <h4>Egg</h4>
+      <div className="egg"/>
+      <h4>Infinity</h4>
+      <div className="infinity"/>
+      <h4>yin-yang</h4> 
+      <div className="yin-yang"/>
+      <h4>Triangle up</h4> 
+      <div className="triangle-up"/>
+      <h4>Oval</h4> 
+      <div className="oval"/>
+      <h4>Diamond</h4> 
+      <div className="diamond"/>
+      <h4>Heart</h4> 
+      <div className="heart"/>
+      
+    </div>
+  );
+}
